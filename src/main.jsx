9ee3ac185a9e9978e5fa8b57ab96b1cb99cc58ee
alisonborba <alisonborbaa@gmail.com>
2023-12-03 14:55:16 +0000

    Revert "changing ThirdwebProvider getting the right chain"
    This reverts commit 854607953e091a6fb558b4957601cafeb20f2d8b.

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import { StateContextProvider } from "./context";
-import App from "./App";
-import "./index.css";
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider
-    activeChain="goerli"
-    clientId="565acaaab1c7a39a3f2433151d9e262e"
-  >
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
     <Router>
       <StateContextProvider>
         <App />
       </StateContextProvider>
     </Router>
-  </ThirdwebProvider>
-);
+  </ThirdwebProvider> 
+)
