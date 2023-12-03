854607953e091a6fb558b4957601cafeb20f2d8b
alisonborba <alisonborbaa@gmail.com>
2023-12-03 14:44:14 +0000

    changing ThirdwebProvider getting the right chain

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-import { StateContextProvider } from './context';
-import App from './App';
-import './index.css';
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+  <ThirdwebProvider
+    activeChain="goerli"
+    clientId="565acaaab1c7a39a3f2433151d9e262e"
+  >
     <Router>
       <StateContextProvider>
         <App />
       </StateContextProvider>
     </Router>
-  </ThirdwebProvider> 
-)
+  </ThirdwebProvider>
+);
